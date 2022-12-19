177cc433519625a9a7b094dad7d9f21b47971355
Elie Michel <elie.michel@exppad.com>
2022-12-19 01:05:39 +0100

    Step 005

--- main.cpp
+++ main.cpp
@@ -26,9 +26,30 @@
 
 #include <GLFW/glfw3.h>
 
+#include <webgpu.h>
+
 #include <iostream>
 
 int main (int, char**) {
+	// We create the equivalent of the navigator.gpu if this were web code
+
+	// 1. We create a descriptor
+	WGPUInstanceDescriptor desc = {};
+	desc.nextInChain = nullptr;
+
+	// 2. We create the instance using this descriptor
+	WGPUInstance instance = wgpuCreateInstance(&desc);
+
+	// 3. We can check whether there is actually an instance created
+	if (!instance) {
+		std::cerr << "Could not initialize WebGPU!" << std::endl;
+		return 1;
+	}
+
+	// 4. Display the object (WGPUInstance is a simple pointer, it may be
+	// copied around without worrying about its size).
+	std::cout << "WGPU instance: " << instance << std::endl;
+
 	if (!glfwInit()) {
 		std::cerr << "Could not initialize GLFW!" << std::endl;
 		return 1;
