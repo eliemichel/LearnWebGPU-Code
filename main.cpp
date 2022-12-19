912804b01cc2ef1b87a663c5d21dcb3b80b212bf
Elie Michel <elie.michel@exppad.com>
2022-12-19 01:07:29 +0100

    Step 018

--- main.cpp
+++ main.cpp
@@ -30,8 +30,10 @@
 #include <GLFW/glfw3.h>
 
 #include <webgpu.h>
+#include <wgpu.h> // wgpuTextureViewDrop
 
 #include <iostream>
+#include <vector>
 #include <cassert>
 
 #define UNUSED(x) (void)x;
@@ -58,26 +60,24 @@ int main (int, char**) {
 	}
 
 	std::cout << "Requesting adapter..." << std::endl;
+	WGPUSurface surface = glfwGetWGPUSurface(instance, window);
 	WGPURequestAdapterOptions adapterOpts = {};
 	adapterOpts.nextInChain = nullptr;
-	adapterOpts.compatibleSurface = glfwGetWGPUSurface(instance, window);
+	adapterOpts.compatibleSurface = surface;
 	WGPUAdapter adapter = requestAdapter(instance, &adapterOpts);
 	std::cout << "Got adapter: " << adapter << std::endl;
 
 	std::cout << "Requesting device..." << std::endl;
-
     WGPUDeviceDescriptor deviceDesc = {};
 	deviceDesc.nextInChain = nullptr;
-	deviceDesc.label = "My Device"; // anything works here, that's your call
-	deviceDesc.requiredFeaturesCount = 0; // we do not require any specific feature
-	deviceDesc.requiredLimits = nullptr; // we do not require any specific limit
+	deviceDesc.label = "My Device";
+	deviceDesc.requiredFeaturesCount = 0;
+	deviceDesc.requiredLimits = nullptr;
 	deviceDesc.defaultQueue.nextInChain = nullptr;
 	deviceDesc.defaultQueue.label = "The default queue";
 	WGPUDevice device = requestDevice(adapter, &deviceDesc);
-
 	std::cout << "Got device: " << device << std::endl;
 
-	// Add a callback that gets executed upon errors in our use of the device
 	auto onDeviceError = [](WGPUErrorType type, char const* message, void* /* pUserData */) {
 		std::cout << "Uncaptured device error: type " << type;
 		if (message) std::cout << " (" << message << ")";
@@ -85,19 +85,113 @@ int main (int, char**) {
 	};
 	wgpuDeviceSetUncapturedErrorCallback(device, onDeviceError, nullptr /* pUserData */);
 
-	inspectDevice(device);
+	std::cout << "Allocating GPU memory..." << std::endl;
+	
+	// Create a first buffer, which we use to upload data to the GPU
+	WGPUBufferDescriptor bufferDesc = {};
+	bufferDesc.nextInChain = nullptr;
+	bufferDesc.label = "Input buffer: Written from the CPU to the GPU";
+	bufferDesc.usage = WGPUBufferUsage_CopyDst | WGPUBufferUsage_CopySrc;
+	bufferDesc.size = 16;
+	bufferDesc.mappedAtCreation = false;
+	WGPUBuffer buffer1 = wgpuDeviceCreateBuffer(device, &bufferDesc);
 
-	wgpuDeviceSetDeviceLostCallback(device, [](WGPUDeviceLostReason reason, char const * message, void*) {
-		std::cout << "Device lost! Reason: " << reason << ", message: " << message << std::endl;
-	}, nullptr);
+	// Create a second buffer, with a `MapRead` usage flag so that we can map it later
+	bufferDesc.nextInChain = nullptr;
+	bufferDesc.label = "Output buffer: Read back from the GPU by the CPU";
+	bufferDesc.usage = WGPUBufferUsage_CopyDst | WGPUBufferUsage_MapRead;
+	bufferDesc.size = 16;
+	bufferDesc.mappedAtCreation = false;
+	WGPUBuffer buffer2 = wgpuDeviceCreateBuffer(device, &bufferDesc);
+
+	std::cout << "Configuring command queue..." << std::endl;
+
+	// Get the command queue, through which we send commands to the GPU
+	WGPUQueue queue = wgpuDeviceGetQueue(device);
+
+	// Add a callback for debugging when commands in the queue have been executed
+#if 0 // not implemented yet by the wgpu-native backend
+	auto onQueueWorkDone = [](WGPUQueueWorkDoneStatus status, void* /* pUserData */) {
+		std::cout << "Queued work finished with status: " << status << std::endl;
+	};
+	wgpuQueueOnSubmittedWorkDone(queue, onQueueWorkDone, nullptr /* pUserData */);
+#endif
+
+	std::cout << "Uploading data to the GPU..." << std::endl;
+
+	// Create some CPU-side data buffer (of size 16 bytes)
+	std::vector<unsigned char> numbers(16);
+	for (unsigned char i = 0; i < 16; ++i) numbers[i] = i;
+
+	// Copy this from `numbers` (RAM) to `buffer1` (VRAM)
+	wgpuQueueWriteBuffer(queue, buffer1, 0, numbers.data(), numbers.size());
+
+	std::cout << "Sending buffer copy operation..." << std::endl;
+
+	// The only way to create a command buffer (which is needed to to anything
+	// else than uploading buffer or texture data) is to use a command encoder.
+	WGPUCommandEncoderDescriptor encoderDesc = {};
+	encoderDesc.nextInChain = nullptr;
+	encoderDesc.label = "Command encoder";
+	WGPUCommandEncoder encoder = wgpuDeviceCreateCommandEncoder(device, &encoderDesc);
+
+	// We add a command to the encoder:
+	//   "Copy the current state of buffer 1 to buffer 2"
+	wgpuCommandEncoderCopyBufferToBuffer(encoder, buffer1, 0, buffer2, 0, 16);
+
+	// Finalize the encoding operation, synthesizing the command buffer
+	WGPUCommandBufferDescriptor cmdBufferDescriptor = {};
+	cmdBufferDescriptor.nextInChain = nullptr;
+	cmdBufferDescriptor.label = "Command buffer";
+	WGPUCommandBuffer command = wgpuCommandEncoderFinish(encoder, &cmdBufferDescriptor);
+	
+	// Submit the encoded command buffer
+	wgpuQueueSubmit(queue, 1, &command);
+
+	std::cout << "Start downloading result data from the GPU..." << std::endl;
+
+	// The context shared between this main function and the callback.
+	struct Context {
+		WGPUBuffer buffer;
+	};
+	auto onBuffer2Mapped = [](WGPUBufferMapAsyncStatus status, void* pUserData) {
+		Context* context = reinterpret_cast<Context*>(pUserData);
+		std::cout << "Buffer 2 mapped with status " << status << std::endl;
+		if (status != WGPUBufferMapAsyncStatus_Success) return;
+
+		// Get a pointer to wherever the driver mapped the GPU memory to the RAM
+		unsigned char* bufferData = (unsigned char*)wgpuBufferGetMappedRange(context->buffer, 0, 16);
+
+		// Do stuff with bufferData
+		std::cout << "bufferData = [";
+		for (unsigned char i = 0; i < 16; ++i) {
+			if (i > 0) std::cout << ", ";
+			std::cout << (int)bufferData[i];
+		}
+		std::cout << "]" << std::endl;
+
+		// Then do not forget to unmap the memory
+		wgpuBufferUnmap(context->buffer);
+	};
+
+	Context context = { buffer2 };
+	wgpuBufferMapAsync(buffer2, WGPUMapMode_Read, 0, 16, onBuffer2Mapped, (void*)&context);
 
 	while (!glfwWindowShouldClose(window)) {
+		// Do nothing, this checks for ongoing asynchronous operations and call their callbacks if needed
+		// NB: Our wgpu-native backend provides a more explicit but non-standard wgpuDevicePoll(device) to do this.
+		wgpuQueueSubmit(queue, 0, nullptr);
+
+		// (This is the same idea, for the GLFW library callbacks)
 		glfwPollEvents();
 	}
 
 	glfwDestroyWindow(window);
 	glfwTerminate();
 
+	// Free GPU memory
+	wgpuBufferDestroy(buffer1);
+	wgpuBufferDestroy(buffer2);
+
 	return 0;
 }
-
