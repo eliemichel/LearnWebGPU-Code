eb3c5720beec583ea857ea2d7b34fa806c7bfa3a
Elie Michel <elie.michel@exppad.com>
2022-12-29 00:52:39 +0100

    Add comments

--- main.cpp
+++ main.cpp
@@ -76,6 +76,8 @@ int main (int, char**) {
 	Device device = adapter.requestDevice(deviceDesc);
 	std::cout << "Got device: " << device << std::endl;
 
+	// Add an error callback for more debug info
+	// (TODO: fix the callback in the webgpu.hpp wrapper)
 	auto myCallback = [](ErrorType type, char const* message) {
 		std::cout << "Device error: type " << type;
 		if (message) std::cout << " (message: " << message << ")";
@@ -104,7 +106,7 @@ int main (int, char**) {
 	SwapChain swapChain = device.createSwapChain(surface, swapChainDesc);
 	std::cout << "Swapchain: " << swapChain << std::endl;
 
-	std::cout << "Creating render pipeline..." << std::endl;
+	std::cout << "Creating shader module..." << std::endl;
 	const char* shaderSource = R"(
 @vertex
 fn vs_main(@builtin(vertex_index) in_vertex_index: u32) -> @builtin(position) vec4<f32> {
@@ -124,61 +126,99 @@ fn fs_main() -> @location(0) vec4<f32> {
 )";
 
 	ShaderModuleDescriptor shaderDesc{};
-	ShaderModuleWGSLDescriptor shaderCodeDesc{};
-	shaderCodeDesc.chain.next = nullptr;
-	shaderCodeDesc.chain.sType = SType::ShaderModuleWGSLDescriptor;
-	shaderCodeDesc.code = shaderSource;
-	shaderDesc.nextInChain = reinterpret_cast<ChainedStruct*>(&shaderCodeDesc);
 	shaderDesc.hintCount = 0;
 	shaderDesc.hints = nullptr;
+
+	// Use the extension mechanism to load a WGSL shader source code
+	ShaderModuleWGSLDescriptor shaderCodeDesc{};
+	// Set the chained struct's header
+	shaderCodeDesc.chain.next = nullptr;
+	shaderCodeDesc.chain.sType = SType::ShaderModuleWGSLDescriptor;
+	// Connect the chain
+	shaderDesc.nextInChain = &shaderCodeDesc.chain;
+
+	// Setup the actual payload of the shader code descriptor
+	shaderCodeDesc.code = shaderSource;
+
 	ShaderModule shaderModule = device.createShaderModule(shaderDesc);
+	std::cout << "Shader module: " << shaderModule << std::endl;
 
-	PipelineLayoutDescriptor layoutDesc{};
-	layoutDesc.bindGroupLayoutCount = 0;
-	layoutDesc.bindGroupLayouts = nullptr;
-	PipelineLayout layout = device.createPipelineLayout(layoutDesc);
-
+	std::cout << "Creating render pipeline..." << std::endl;
 	RenderPipelineDescriptor pipelineDesc{};
-	pipelineDesc.layout = layout;
 
+	// Vertex fetch
+	// (We don't use any input buffer so far)
+	pipelineDesc.vertex.bufferCount = 0;
+	pipelineDesc.vertex.buffers = nullptr;
+
+	// Vertex shader
 	pipelineDesc.vertex.module = shaderModule;
 	pipelineDesc.vertex.entryPoint = "vs_main";
 	pipelineDesc.vertex.constantCount = 0;
 	pipelineDesc.vertex.constants = nullptr;
-	pipelineDesc.vertex.bufferCount = 0;
-	pipelineDesc.vertex.buffers = nullptr;
 
+	// Primitive assembly and rasterization
+	// Each sequence of 3 vertices is considered as a triangle
 	pipelineDesc.primitive.topology = PrimitiveTopology::TriangleList;
+	// We'll see later how to specify the order in which vertices should be
+	// connected. When not specified, vertices are considered sequentially.
 	pipelineDesc.primitive.stripIndexFormat = IndexFormat::Undefined;
+	// The face orientation is defined by assuming that when looking
+	// from the front of the face, its corner vertices are enumerated
+	// in the counter-clockwise (CCW) order.
 	pipelineDesc.primitive.frontFace = FrontFace::CCW;
-	pipelineDesc.primitive.cullMode = CullMode::None; // usually we set it to Front to alleviate rendering, but for beginners it can be very frustrating to see nothing on screen for hours only to discover that the triangle was just facing in the wrong direction.
+	// But the face orientation does not matter much because we do not
+	// cull (i.e. "hide") the faces pointing away from us (which is often
+	// used for optimization).
+	pipelineDesc.primitive.cullMode = CullMode::None;
 
-	BlendState blendState{};
-	blendState.color.srcFactor = BlendFactor::One;
-	blendState.color.dstFactor = BlendFactor::Zero;
-	blendState.color.operation = BlendOperation::Add;
-	blendState.alpha.srcFactor = BlendFactor::SrcAlpha;
-	blendState.alpha.dstFactor = BlendFactor::OneMinusSrcAlpha;
-	blendState.alpha.operation = BlendOperation::Add;
-	ColorTargetState colorTarget{};
-	colorTarget.format = swapChainFormat;
-	colorTarget.blend = &blendState;
-	colorTarget.writeMask = ColorWriteMask::All;
-
+	// Fragment shader
 	FragmentState fragmentState{};
+	pipelineDesc.fragment = &fragmentState;
 	fragmentState.module = shaderModule;
 	fragmentState.entryPoint = "fs_main";
 	fragmentState.constantCount = 0;
 	fragmentState.constants = nullptr;
+
+	// Configure blend state
+	BlendState blendState{};
+	// Usual alpha blending for the color:
+	blendState.color.srcFactor = BlendFactor::SrcAlpha;
+	blendState.color.dstFactor = BlendFactor::OneMinusSrcAlpha;
+	blendState.color.operation = BlendOperation::Add;
+	// We leave the target alpha untouched:
+	blendState.alpha.srcFactor = BlendFactor::Zero;
+	blendState.alpha.dstFactor = BlendFactor::One;
+	blendState.alpha.operation = BlendOperation::Add;
+
+	ColorTargetState colorTarget{};
+	colorTarget.format = swapChainFormat;
+	colorTarget.blend = &blendState;
+	colorTarget.writeMask = ColorWriteMask::All; // We could write to only some of the color channels.
+
+	// We have only one target because our render pass has only one output color
+	// attachment.
 	fragmentState.targetCount = 1;
 	fragmentState.targets = &colorTarget;
-	pipelineDesc.fragment = &fragmentState;
-
+	
+	// Depth and stencil tests are not used here
 	pipelineDesc.depthStencil = nullptr;
 
+	// Multi-sampling
+	// Samples per pixel
 	pipelineDesc.multisample.count = 1;
-	pipelineDesc.multisample.mask = ~0u; // meaning "all bits on"
+	// Default value for the mask, meaning "all bits on"
+	pipelineDesc.multisample.mask = ~0u;
+	// Default value as well (irrelevant for count = 1 anyways)
 	pipelineDesc.multisample.alphaToCoverageEnabled = false;
+
+	// Pipeline layout
+	// (Our example does not use any resource)
+	PipelineLayoutDescriptor layoutDesc{};
+	layoutDesc.bindGroupLayoutCount = 0;
+	layoutDesc.bindGroupLayouts = nullptr;
+	PipelineLayout layout = device.createPipelineLayout(layoutDesc);
+	pipelineDesc.layout = layout;
 
 	RenderPipeline pipeline = device.createRenderPipeline(pipelineDesc);
 	std::cout << "Render pipeline: " << pipeline << std::endl;
@@ -212,7 +252,10 @@ fn fs_main() -> @location(0) vec4<f32> {
 		renderPassDesc.timestampWrites = nullptr;
 		RenderPassEncoder renderPass = encoder.beginRenderPass(renderPassDesc);
 
+		// In its overall outline, drawing a triangle is as simple as this:
+		// Select which render pipeline to use
 		renderPass.setPipeline(pipeline);
+		// Draw 1 instance of a 3-vertices shape
 		renderPass.draw(3, 1, 0, 0);
 
 		renderPass.end();
