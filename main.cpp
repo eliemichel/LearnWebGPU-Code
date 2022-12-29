02d1cec08b4a1888ac24c71107119b1a944c9264
Elie Michel <elie.michel@exppad.com>
2022-12-29 18:51:25 +0100

    Slight change to base shader

--- main.cpp
+++ main.cpp
@@ -110,13 +110,15 @@ int main (int, char**) {
 	const char* shaderSource = R"(
 @vertex
 fn vs_main(@builtin(vertex_index) in_vertex_index: u32) -> @builtin(position) vec4<f32> {
+	var p = vec2<f32>(0.0, 0.0);
 	if (in_vertex_index == 0u) {
-		return vec4<f32>(-0.5, -0.5, 0.0, 1.0);
+		p = vec2<f32>(-0.5, -0.5);
 	} else if (in_vertex_index == 1u) {
-		return vec4<f32>(0.5, -0.5, 0.0, 1.0);
+		p = vec2<f32>(0.5, -0.5);
 	} else {
-		return vec4<f32>(0.0, 0.5, 0.0, 1.0);
+		p = vec2<f32>(0.0, 0.5);
 	}
+	return vec4<f32>(p, 0.0, 1.0);
 }
 
 @fragment
