3193990fcb2213d3940cb8205f9db2853567cce4
Elie Michel <elie.michel@exppad.com>
2023-07-06 08:42:03 +0200

    Minor fix

--- main.cpp
+++ main.cpp
@@ -311,7 +311,7 @@ int main (int, char**) {
 		
 		RenderPassDescriptor renderPassDesc{};
 
-		WGPURenderPassColorAttachment renderPassColorAttachment;
+		RenderPassColorAttachment renderPassColorAttachment{};
 		renderPassColorAttachment.view = nextTexture;
 		renderPassColorAttachment.resolveTarget = nullptr;
 		renderPassColorAttachment.loadOp = LoadOp::Clear;
