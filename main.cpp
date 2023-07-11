217730ae36a4f9ccffdedb7089470945270ff891
Elie Michel <elie.michel@exppad.com>
2023-07-11 08:39:34 +0200

    Fix cast deref ordering

--- main.cpp
+++ main.cpp
@@ -227,7 +227,7 @@ int main (int, char**) {
 	// Create the pipeline layout
 	PipelineLayoutDescriptor layoutDesc;
 	layoutDesc.bindGroupLayoutCount = 1;
-	layoutDesc.bindGroupLayouts = &(WGPUBindGroupLayout)bindGroupLayout;
+	layoutDesc.bindGroupLayouts = (WGPUBindGroupLayout*)&bindGroupLayout;
 	PipelineLayout layout = device.createPipelineLayout(layoutDesc);
 	pipelineDesc.layout = layout;
 
