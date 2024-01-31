87c7b68a4b81f91144a25d00cad8d40524211915
Elie Michel <elie.michel@exppad.com>
2024-01-31 08:36:45 +0100

    Fix surface release

--- main.cpp
+++ main.cpp
@@ -94,6 +94,7 @@ int main (int, char**) {
 	wgpuDeviceRelease(device);
 	wgpuAdapterRelease(adapter);
 	wgpuInstanceRelease(instance);
+	wgpuSurfaceRelease(surface);
 	glfwDestroyWindow(window);
 	glfwTerminate();
 
