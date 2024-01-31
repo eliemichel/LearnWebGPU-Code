c3833839ce80a90bb42af184002c28ed1c985150
Elie Michel <elie.michel@exppad.com>
2024-01-31 08:58:46 +0100

    Add missing releases

--- Application.cpp
+++ Application.cpp
@@ -131,13 +131,16 @@ void Application::onFrame() {
 	updateGui(renderPass);
 
 	renderPass.end();
+	renderPass.release();
 	
 	nextTexture.release();
 
 	CommandBufferDescriptor cmdBufferDescriptor{};
 	cmdBufferDescriptor.label = "Command buffer";
 	CommandBuffer command = encoder.finish(cmdBufferDescriptor);
+	encoder.release();
 	m_queue.submit(command);
+	command.release();
 
 	m_swapChain.present();
 
@@ -279,7 +282,6 @@ bool Application::initWindowAndDevice() {
 	deviceDesc.requiredLimits = &requiredLimits;
 	deviceDesc.defaultQueue.label = "The default queue";
 	m_device = adapter.requestDevice(deviceDesc);
-	adapter.release();
 	std::cout << "Got device: " << m_device << std::endl;
 
 	// Add an error callback for more debug info
@@ -317,6 +319,7 @@ bool Application::initWindowAndDevice() {
 		if (that != nullptr) that->onScroll(xoffset, yoffset);
 	});
 
+	adapter.release();
 	return m_device != nullptr;
 }
 
