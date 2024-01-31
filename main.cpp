ffa0fb08cf9d5a4b542fe9d6399705d873bcd318
Elie Michel <elie.michel@exppad.com>
2024-01-31 09:50:19 +0100

    Fix missing merge

--- main.cpp
+++ main.cpp
@@ -596,8 +596,6 @@ int main (int, char**) {
 		command.release();
 
 		swapChain.present();
->>>>>>> c21c10a (Fix render pass release)
->>>>>>> fc12322 (Fix render pass release)
 	}
 
 	vertexBuffer.destroy();
