e5bcb41a72ce1c72be034f0bfa84815a126943cf
Elie Michel <elie.michel@exppad.com>
2024-01-31 08:58:46 +0100

    Add missing releases

--- main.cpp
+++ main.cpp
@@ -511,7 +511,9 @@ int main (int, char**) {
 		CommandBufferDescriptor cmdBufferDescriptor{};
 		cmdBufferDescriptor.label = "Command buffer";
 		CommandBuffer command = encoder.finish(cmdBufferDescriptor);
+		encoder.release();
 		queue.submit(command);
+		command.release();
 
 		swapChain.present();
 
@@ -526,15 +528,18 @@ int main (int, char**) {
 	indexBuffer.destroy();
 	indexBuffer.release();
 
+	// Destroy the depth texture and its view
 	depthTextureView.release();
 	depthTexture.destroy();
 	depthTexture.release();
 
+	pipeline.release();
+	shaderModule.release();
 	swapChain.release();
 	device.release();
 	adapter.release();
 	instance.release();
-
+	surface.release();
 	glfwDestroyWindow(window);
 	glfwTerminate();
 
