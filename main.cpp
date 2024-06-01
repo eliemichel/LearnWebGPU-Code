15634c05db8b25b38581659d484673e6c0738c45
Elie Michel <elie.michel@exppad.com>
2024-06-01 18:35:33 +0200

    Upgrade to WebGPU distribution v0.2.0

--- main.cpp
+++ main.cpp
@@ -1,20 +1,20 @@
 /**
  * This file is part of the "Learn WebGPU for C++" book.
  *   https://github.com/eliemichel/LearnWebGPU
- * 
+ *
  * MIT License
- * Copyright (c) 2022-2023 Elie Michel
- * 
+ * Copyright (c) 2022-2024 Elie Michel
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -24,10 +24,11 @@
  * SOFTWARE.
  */
 
+// This is a standard C++ "Hello world"
+
 #include <iostream>
 
 int main (int, char**) {
 	std::cout << "Hello, world!" << std::endl;
 	return 0;
 }
-
