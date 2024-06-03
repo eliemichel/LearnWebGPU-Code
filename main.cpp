3cfea94bb1d261286072331b06221896e8a80cde
Elie Michel <elie.michel@exppad.com>
2024-06-04 00:14:59 +0200

    Add a first render pipeline

--- main.cpp
+++ main.cpp
@@ -13,8 +13,28 @@
 #include <cassert>
 #include <vector>
 
-// Avoid the "wgpu::" prefix in front of all WebGPU symbols
 using namespace wgpu;
+
+// We embbed the source of the shader module here
+const char* shaderSource = R"(
+@vertex
+fn vs_main(@builtin(vertex_index) in_vertex_index: u32) -> @builtin(position) vec4f {
+	var p = vec2f(0.0, 0.0);
+	if (in_vertex_index == 0u) {
+		p = vec2f(-0.5, -0.5);
+	} else if (in_vertex_index == 1u) {
+		p = vec2f(0.5, -0.5);
+	} else {
+		p = vec2f(0.0, 0.5);
+	}
+	return vec4f(p, 0.0, 1.0);
+}
+
+@fragment
+fn fs_main() -> @location(0) vec4f {
+	return vec4f(0.0, 0.4, 1.0, 1.0);
+}
+)";
 
 class Application {
 public:
@@ -33,12 +53,17 @@ public:
 private:
 	TextureView GetNextSurfaceTextureView();
 
+	// Substep of Initialize() that creates the render pipeline
+	void InitializePipeline();
+
 private:
 	// We put here all the variables that are shared between init and main loop
 	GLFWwindow *window;
 	Device device;
 	Queue queue;
 	Surface surface;
+	TextureFormat surfaceFormat = TextureFormat::Undefined;
+	RenderPipeline pipeline;
 };
 
 int main() {
@@ -116,7 +141,7 @@ bool Application::Initialize() {
 	config.width = 640;
 	config.height = 480;
 	config.usage = TextureUsage::RenderAttachment;
-	TextureFormat surfaceFormat = surface.getPreferredFormat(adapter);
+	surfaceFormat = surface.getPreferredFormat(adapter);
 	config.format = surfaceFormat;
 
 	// And we do not need any particular view format:
@@ -128,10 +153,13 @@ bool Application::Initialize() {
 
 	surface.configure(config);
 
+	InitializePipeline();
+
 	return true;
 }
 
 void Application::Terminate() {
+	pipeline.release();
 	surface.unconfigure();
 	queue.release();
 	surface.release();
@@ -171,8 +199,13 @@ void Application::MainLoop() {
 	renderPassDesc.depthStencilAttachment = nullptr;
 	renderPassDesc.timestampWrites = nullptr;
 
-	// Create the render pass and end it immediately (we only clear the screen but do not draw anything)
 	RenderPassEncoder renderPass = encoder.beginRenderPass(renderPassDesc);
+
+	// Select which render pipeline to use
+	renderPass.setPipeline(pipeline);
+	// Draw 1 instance of a 3-vertices shape
+	renderPass.draw(3, 1, 0, 0);
+
 	renderPass.end();
 	renderPass.release();
 
@@ -226,4 +259,100 @@ TextureView Application::GetNextSurfaceTextureView() {
 	TextureView targetView = texture.createView(viewDescriptor);
 
 	return targetView;
+}
+
+void Application::InitializePipeline() {
+	// Load the shader module
+	ShaderModuleDescriptor shaderDesc;
+#ifdef WEBGPU_BACKEND_WGPU
+	shaderDesc.hintCount = 0;
+	shaderDesc.hints = nullptr;
+#endif
+
+	// We use the extension mechanism to specify the WGSL part of the shader module descriptor
+	ShaderModuleWGSLDescriptor shaderCodeDesc;
+	// Set the chained struct's header
+	shaderCodeDesc.chain.next = nullptr;
+	shaderCodeDesc.chain.sType = SType::ShaderModuleWGSLDescriptor;
+	// Connect the chain
+	shaderDesc.nextInChain = &shaderCodeDesc.chain;
+	shaderCodeDesc.code = shaderSource;
+	ShaderModule shaderModule = device.createShaderModule(shaderDesc);
+
+	// Create the render pipeline
+	RenderPipelineDescriptor pipelineDesc;
+
+	// We do not use any vertex buffer for this first simplistic example
+	pipelineDesc.vertex.bufferCount = 0;
+	pipelineDesc.vertex.buffers = nullptr;
+
+	// NB: We define the 'shaderModule' in the second part of this chapter.
+	// Here we tell that the programmable vertex shader stage is described
+	// by the function called 'vs_main' in that module.
+	pipelineDesc.vertex.module = shaderModule;
+	pipelineDesc.vertex.entryPoint = "vs_main";
+	pipelineDesc.vertex.constantCount = 0;
+	pipelineDesc.vertex.constants = nullptr;
+
+	// Each sequence of 3 vertices is considered as a triangle
+	pipelineDesc.primitive.topology = PrimitiveTopology::TriangleList;
+	
+	// We'll see later how to specify the order in which vertices should be
+	// connected. When not specified, vertices are considered sequentially.
+	pipelineDesc.primitive.stripIndexFormat = IndexFormat::Undefined;
+	
+	// The face orientation is defined by assuming that when looking
+	// from the front of the face, its corner vertices are enumerated
+	// in the counter-clockwise (CCW) order.
+	pipelineDesc.primitive.frontFace = FrontFace::CCW;
+	
+	// But the face orientation does not matter much because we do not
+	// cull (i.e. "hide") the faces pointing away from us (which is often
+	// used for optimization).
+	pipelineDesc.primitive.cullMode = CullMode::None;
+
+	// We tell that the programmable fragment shader stage is described
+	// by the function called 'fs_main' in the shader module.
+	FragmentState fragmentState;
+	fragmentState.module = shaderModule;
+	fragmentState.entryPoint = "fs_main";
+	fragmentState.constantCount = 0;
+	fragmentState.constants = nullptr;
+
+	BlendState blendState;
+	blendState.color.srcFactor = BlendFactor::SrcAlpha;
+	blendState.color.dstFactor = BlendFactor::OneMinusSrcAlpha;
+	blendState.color.operation = BlendOperation::Add;
+	blendState.alpha.srcFactor = BlendFactor::Zero;
+	blendState.alpha.dstFactor = BlendFactor::One;
+	blendState.alpha.operation = BlendOperation::Add;
+	
+	ColorTargetState colorTarget;
+	colorTarget.format = surfaceFormat;
+	colorTarget.blend = &blendState;
+	colorTarget.writeMask = ColorWriteMask::All; // We could write to only some of the color channels.
+	
+	// We have only one target because our render pass has only one output color
+	// attachment.
+	fragmentState.targetCount = 1;
+	fragmentState.targets = &colorTarget;
+	pipelineDesc.fragment = &fragmentState;
+
+	// We do not use stencil/depth testing for now
+	pipelineDesc.depthStencil = nullptr;
+
+	// Samples per pixel
+	pipelineDesc.multisample.count = 1;
+
+	// Default value for the mask, meaning "all bits on"
+	pipelineDesc.multisample.mask = ~0u;
+
+	// Default value as well (irrelevant for count = 1 anyways)
+	pipelineDesc.multisample.alphaToCoverageEnabled = false;
+	pipelineDesc.layout = nullptr;
+	
+	pipeline = device.createRenderPipeline(pipelineDesc);
+
+	// We no longer need to access the shader module
+	shaderModule.release();
 }
