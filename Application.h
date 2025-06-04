358690861315082d9870ce804ff30cd05902e5cc
Elie Michel <elie.michel@exppad.com>
2025-06-04 09:07:38 +0200

    Auto update from 'Upgrade Next to WebGPU distrib v0.3.0-gamma'

--- Application.h
+++ Application.h
@@ -37,6 +37,6 @@ private:
 	private: // In Application.h
 		wgpu::TextureFormat m_surfaceFormat = wgpu::TextureFormat::Undefined;
 	private: // Application attributes
-		wgpu::Buffer m_vertexBuffer;
-		uint32_t m_vertexCount;
+		wgpu::Buffer m_vertexBuffer = nullptr;
+		uint32_t m_vertexCount = 0;
 };
